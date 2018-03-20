f99e036bdbda9652f90494ed54f803d6bdec47d4
Jacob Fletcher <jacobsfletch@gmail.com>
2018-03-20 12:28:36 -0400

    simplifies svg datasets

--- src/Graph.js
+++ src/Graph.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Graph = (props) => {
 	return (
-		<svg x="0px" y="0px" viewBox="0 0 809 9">
+		<svg x="0px" y="0px" viewBox="0 0 1000 2">
 			<polyline points={props.polyline} />
 			{props.arrayX.map((coordX, i) => {
 				return (
